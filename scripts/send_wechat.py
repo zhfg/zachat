cec723e13b1dcd92b6203372b793dc8bb2f44448
jacob <angell.zhfg@gmail.com>
2017-08-22 08:54:48 +0800

    update server url

--- scripts/send_wechat.py
+++ scripts/send_wechat.py
@@ -6,7 +6,7 @@ import os, sys,logging
 
 #http://127.0.0.1:6543/?message=asdfasdf%3Basdfasdf%0Aeiasjdf%3Baksdjfa%3Bskldfj%0Aasdfeijasd%3Bfkajsdf%0Aeiajs%3Bdflkajeiaspeoifjasdfa%0Aasdfieja%3Bsdkfjas%3Bfiejaieojasdfasdf%0A&group=zabbix%E5%BE%AE%E4%BF%A1%E6%8A%A5%E8%AD%A6%E6%B5%8B%E8%AF%95
 
-url = 'http://127.0.0.1:6543/send'
+url = 'http://222.92.56.244:7788/send'
 argc = len(sys.argv)
 if argc < 3:
     print("Need less 2 argments, %d givened" % (argc-1))
